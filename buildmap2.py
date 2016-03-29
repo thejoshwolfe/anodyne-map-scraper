2b6cc141052c30d1c36b788e625b46b113b82838
Josh Wolfe <thejoshwolfe@gmail.com>
2016-03-29 10:19:12 -0700

    option to output separate layers

--- buildmap2.py
+++ buildmap2.py
@@ -15,6 +15,7 @@
 import os
 import csv
 import simplepng
+from collections import defaultdict
 from xml import sax
 
 def read_layer(filename):
@@ -34,38 +35,43 @@ def read_tileset(filename):
 def paint_with_layer(image, layer, tileset):
   y_blocks = len(layer)
   x_blocks = len(layer[0])
+  found_anything = False
   for y in range(y_blocks):
     for x in range(x_blocks):
       tile_index = int(layer[y][x])
+      if tile_index == 0: continue
+      found_anything = True
       tile_y = tile_index // (tileset.width // 16)
       tile_x = tile_index % (tileset.width // 16)
-      if tile_index != 0:
-        image.paste(tileset, sx=tile_x*16, sy=tile_y*16, dx=x*16, dy=y*16, width=16, height=16)
+      image.paste(tileset, sx=tile_x*16, sy=tile_y*16, dx=x*16, dy=y*16, width=16, height=16)
+  return found_anything
 
 
 def generate_map_image(map):
-  layer = read_layer(map["layers"][0])
+  layer_images = [None, None, None, None]
   tileset = read_tileset(map["tileset"])
-  image = simplepng.ImageBuffer(len(layer[0]) * 16, len(layer) * 16)
-  paint_with_layer(image, layer, tileset)
-  for layerfile in map["layers"][1:]:
+  width = None
+  height = None
+  for i, layerfile in enumerate(map["layers"]):
     layer = read_layer(layerfile)
-    paint_with_layer(image, layer, tileset)
-  return image
+    if width == None:
+      width = len(layer[0]) * 16
+      height = len(layer) * 16
+    image = simplepng.ImageBuffer(width, height)
+    if paint_with_layer(image, layer, tileset):
+      if i == 2: i = 3 # leave space for the entities layer
+      layer_images[i] = image
+  return layer_images
 
 class RegistryHandler(sax.ContentHandler):
   mapname = ""
-  registry = {}
+  registry = defaultdict(lambda: defaultdict(list))
   def startElement(self, name, attrs):
     if name == "root":
       pass
     elif name == "map":
       self.mapname = attrs["name"]
-      if self.mapname not in self.registry:
-        self.registry[self.mapname] = {}
     else:
-      if name not in self.registry[self.mapname]:
-        self.registry[self.mapname][name] = []
       self.registry[self.mapname][name].append({"x": attrs["x"], "y": attrs["y"], "frame": attrs["frame"]})
   def endElement(self, name):
     pass
@@ -81,127 +87,127 @@ def read_registry():
 
 mapfiles = [
   {
-    "world": "APARTMENT",
+    "map_name": "APARTMENT",
     "tileset": "Anodyne/src/data/TileData__Apartment_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_APARTMENT_BG.dat", "Anodyne/src/data/CSV_Data_APARTMENT_BG2.dat", "Anodyne/src/data/CSV_Data_APARTMENT_FG.dat"]
   },
   {
-    "world": "BEACH",
+    "map_name": "BEACH",
     "tileset": "Anodyne/src/data/TileData__Beach_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_BEACH_BG.dat"]
   },
   {
-    "world": "BEDROOM",
+    "map_name": "BEDROOM",
     "tileset": "Anodyne/src/data/TileData__Bedroom_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_BEDROOM_BG.dat"]
   },
   {
-    "world": "BLANK",
+    "map_name": "BLANK",
     "tileset": "Anodyne/src/data/TileData_Blank_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_BLANK_BG.dat"]
   },
   {
-    "world": "BLUE",
+    "map_name": "BLUE",
     "tileset": "Anodyne/src/data/TileData_Blue_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_BLUE_BG.dat", "Anodyne/src/data/CSV_Data_BLUE_BG2.dat"]
   },
   {
-    "world": "CELL",
+    "map_name": "CELL",
     "tileset": "Anodyne/src/data/TileData_Cell_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_CELL_BG.dat"]
   },
   {
-    "world": "CIRCUS",
+    "map_name": "CIRCUS",
     "tileset": "Anodyne/src/data/TileData__Circus_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_CIRCUS_BG.dat", "Anodyne/src/data/CSV_Data_CIRCUS_FG.dat"]
   },
   {
-    "world": "CLIFF",
+    "map_name": "CLIFF",
     "tileset": "Anodyne/src/data/TileData_Cliff_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_CLIFF_BG.dat", "Anodyne/src/data/CSV_Data_CLIFF_BG2.dat"]
   },
   {
-    "world": "CROWD",
+    "map_name": "CROWD",
     "tileset": "Anodyne/src/data/TileData__Crowd_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_CROWD_BG.dat", "Anodyne/src/data/CSV_Data_CROWD_BG2.dat"]
   },
   {
-    "world": "DEBUG",
+    "map_name": "DEBUG",
     "tileset": "Anodyne/src/data/TileData_Debug_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_DEBUG_BG.dat", "Anodyne/src/data/CSV_Data_DEBUG_BG2.dat", "Anodyne/src/data/CSV_Data_DEBUG_FG.dat"]
   },
   {
-    "world": "DRAWER",
+    "map_name": "DRAWER",
     "tileset": "Anodyne/src/data/TileData_BlackWhite_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_DRAWER_BG.dat"]
   },
   {
-    "world": "FIELDS",
+    "map_name": "FIELDS",
     "tileset": "Anodyne/src/data/TileData__Fields_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_FIELDS_BG.dat", "Anodyne/src/data/CSV_Data_FIELDS_FG.dat"]
   },
   {
-    "world": "FOREST",
+    "map_name": "FOREST",
     "tileset": "Anodyne/src/data/TileData_Forest_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_FOREST_BG.dat", "Anodyne/src/data/CSV_Data_FOREST_BG2.dat", "Anodyne/src/data/CSV_Data_FOREST_FG.dat"]
   },
   {
-    "world": "GO",
+    "map_name": "GO",
     "tileset": "Anodyne/src/data/TileData_Go_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_GO_BG.dat", "Anodyne/src/data/CSV_Data_GO_BG2.dat"]
   },
   {
-    "world": "HAPPY",
+    "map_name": "HAPPY",
     "tileset": "Anodyne/src/data/TileData_Happy_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_HAPPY_BG.dat", "Anodyne/src/data/CSV_Data_HAPPY_BG2.dat"]
   },
   {
-    "world": "HOTEL",
+    "map_name": "HOTEL",
     "tileset": "Anodyne/src/data/TileData__Hotel_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_HOTEL_BG.dat", "Anodyne/src/data/CSV_Data_HOTEL_BG2.dat", "Anodyne/src/data/CSV_Data_HOTEL_FG.dat"]
   },
   {
-    "world": "NEXUS",
+    "map_name": "NEXUS",
     "tileset": "Anodyne/src/data/TileData__Nexus_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_NEXUS_BG.dat", "Anodyne/src/data/CSV_Data_NEXUS_FG.dat"]
   },
   {
-    "world": "OVERWORLD",
+    "map_name": "OVERWORLD",
     "tileset": "Anodyne/src/data/TileData__Overworld_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_OVERWORLD_BG.dat", "Anodyne/src/data/CSV_Data_OVERWORLD_BG2.dat"]
   },
   {
-    "world": "REDCAVE",
+    "map_name": "REDCAVE",
     "tileset": "Anodyne/src/data/TileData_REDCAVE_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_REDCAVE_BG.dat", "Anodyne/src/data/CSV_Data_REDCAVE_BG2.dat"]
   },
   {
-    "world": "REDSEA",
+    "map_name": "REDSEA",
     "tileset": "Anodyne/src/data/TileData_Red_Sea_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_REDSEA_BG.dat", "Anodyne/src/data/CSV_Data_REDSEA_FG.dat"]
   },
   {
-    "world": "SPACE",
+    "map_name": "SPACE",
     "tileset": "Anodyne/src/data/TileData_Space_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_SPACE_BG.dat", "Anodyne/src/data/CSV_Data_SPACE_BG2.dat", "Anodyne/src/data/CSV_Data_SPACE_FG.dat"]
   },
   {
-    "world": "STREET",
+    "map_name": "STREET",
     "tileset": "Anodyne/src/data/TileData__Street_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_STREET_BG.dat", "Anodyne/src/data/CSV_Data_STREET_BG2.dat", "Anodyne/src/data/CSV_Data_STREET_FG.dat"]
   },
   {
-    "world": "SUBURB",
+    "map_name": "SUBURB",
     "tileset": "Anodyne/src/data/TileData_Suburb_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_SUBURB_BG.dat"]
   },
   {
-    "world": "TERMINAL",
+    "map_name": "TERMINAL",
     "tileset": "Anodyne/src/data/TileData_Terminal_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_TERMINAL_BG.dat", "Anodyne/src/data/CSV_Data_TERMINAL_BG2.dat"]
   },
   {
-    "world": "WINDMILL",
+    "map_name": "WINDMILL",
     "tileset": "Anodyne/src/data/TileData__Windmill_Tiles.png",
     "layers": ["Anodyne/src/data/CSV_Data_WINDMILL_BG.dat", "Anodyne/src/data/CSV_Data_WINDMILL_BG2.dat"]
   }
@@ -211,36 +217,52 @@ entities = {
   "Treasure": { "image": "Anodyne/src/entity/gadget/Treasure_S_TREASURE_SPRITE.png", "tile_index": 1}
 }
 
-# read registry XML file that contains entity information
-registry = read_registry()
-treasure_tiles = read_tileset("Anodyne/src/entity/gadget/Treasure_S_TREASURE_SPRITE.png");
+def main():
+  import argparse
+  parser = argparse.ArgumentParser()
+  parser.add_argument("map_name", nargs="*")
+  parser.add_argument("-s", "--separate", action="store_true")
+  args = parser.parse_args()
 
-# build initial map images
-maps = {}
-for mapfile in mapfiles:
-  print("Processing: " + mapfile["world"])
-  map = generate_map_image(mapfile)
-  maps[mapfile["world"]] = map
+  # read registry XML file that contains entity information
+  registry = read_registry()
+  treasure_tiles = read_tileset("Anodyne/src/entity/gadget/Treasure_S_TREASURE_SPRITE.png");
 
-# draw the supported entites on the maps
-for worlds in registry:
-  print("Processing entities: " + worlds)
-  if worlds in maps:
-    apartment = maps[worlds]
-    apartment_regs = registry[worlds]
-    if "Treasure" in apartment_regs:
-      treasures = apartment_regs["Treasure"]
-      for treasure in treasures:
-        x = int(treasure["x"])
-        y = int(treasure["y"])
-        apartment.paste(treasure_tiles, sx=0, sy=0, dx=x, dy=y, width=16, height=16)
+  if not os.path.exists("maps"):
+    os.makedirs("maps")
 
-# Make map directory and save images
-if not os.path.exists("maps"):
-  os.makedirs("maps")
+  for mapfile in mapfiles:
+    map_name = mapfile["map_name"]
+    if args.map_name and map_name not in args.map_name:
+      continue
+    print("Processing: " + map_name)
 
-for map in maps:
-  map_image = maps[map];
-  print("Saving " + map)
-  with open("maps/" + map + ".png", "wb") as f:
-    simplepng.write_png(f, map_image)
+    # build initial map image
+    layers = generate_map_image(mapfile)
+    # draw the supported entites on the maps
+    apartment_regs = registry[map_name]
+    treasures = apartment_regs["Treasure"]
+    for treasure in treasures:
+      x = int(treasure["x"])
+      y = int(treasure["y"])
+      if layers[2] == None:
+        layers[2] = simplepng.ImageBuffer(layers[0].width, layers[0].height)
+      layers[2].paste(treasure_tiles, sx=0, sy=0, dx=x, dy=y, width=16, height=16)
+
+    # save images
+    file_name_base = "maps/" + map_name
+    if args.separate:
+      for i, layer in enumerate(layers):
+        if layer == None: continue
+        with open("{}_{}.png".format(file_name_base, i), "wb") as f:
+          simplepng.write_png(f, layer)
+    else:
+      # add everything to layer[0]
+      for layer in layers[1:]:
+        if layer == None: continue
+        layers[0].paste(layer)
+      with open(file_name_base + ".png", "wb") as f:
+        simplepng.write_png(f, layers[0])
+
+if __name__ == "__main__":
+  main()
